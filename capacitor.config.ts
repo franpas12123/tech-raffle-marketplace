159291568787adad8b3030f147739ec3f42cd999
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-13 21:35:44 +0800

    change appId in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.website.rapol',
   appName: 'rapol-website',
   webDir: 'dist',
   bundledWebRuntime: false
