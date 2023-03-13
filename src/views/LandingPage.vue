075ab454c408a448c9606d639272e7ff9d39e8a6
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-13 22:39:02 +0800

    add landing

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <Layout>
+    <template v-slot:content>
+      <h1 class="ion-text-center">landing page</h1>
+    </template>
+  </Layout>
+</template>
+
+<script setup lang="ts">
+import Layout from '@/layouts/DefaultLayout.vue';
+</script>
