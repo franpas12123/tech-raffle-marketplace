4603ddbe74d750a63c4bcffe39ddb695c900dfea
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-13 22:38:50 +0800

    add footer

---
+++ src/components/footer/FooterContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <ion-footer>
+    <ion-toolbar>
+      <ion-title>Footer</ion-title>
+    </ion-toolbar>
+  </ion-footer>
+</template>
+
+<script setup lang="ts">
+import { IonFooter, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
