28fede5f27172f8d5d5503291ad10bf699848889
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-13 22:38:01 +0800

    add default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-app>
+    <HeaderContainer />
+
+    <ion-content class="ion-padding">
+      <slot name="content">Fallback</slot>
+    </ion-content>
+
+    <FooterContainer />
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonApp } from '@ionic/vue';
+import HeaderContainer from '@/components/header/HeaderContainer.vue'
+import FooterContainer from '@/components/footer/FooterContainer.vue'
+</script>
