b43c5b6d63074b86f45583536bd889e1e44d77f4
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-18 13:02:30 +0800

    import main custom sass

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* SASS stuff */
+import './assets/styles/main.scss';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
