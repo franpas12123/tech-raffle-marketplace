de0bed73179de80aa1f497a61a02733518772b8a
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-18 18:15:09 +0800

    add icons and change style

--- src/components/header/HeaderContainer.vue
+++ src/components/header/HeaderContainer.vue
@@ -1,17 +1,17 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-grid>
+      <ion-grid :fixed="true">
         <ion-row class="ion-align-items-center">
-          <ion-col size="1.5">
+          <ion-col size-lg="2" class="ion-justify-content-start">
             <ion-row>
               <ion-col>
                 <ion-title>RAPOL</ion-title>
               </ion-col>
             </ion-row>
           </ion-col>
-          <ion-col>
-            <ion-row class="ion-justify-content-start ion-align-items-center">
+          <ion-col class="ion-hide-lg-down">
+            <ion-row class="ion-justify-content-center ion-align-items-center">
               <ion-col>
                 Prizes
               </ion-col>
@@ -29,12 +29,18 @@
               </ion-col>
             </ion-row>
           </ion-col>
-          <ion-col></ion-col>
-          <ion-col size="1">
-            login/signup
+          <ion-col size="1"></ion-col>
+          <ion-col size="2">
+            <ion-button color="light" aria-label="Login/Signup">
+              login / signup
+              <ion-icon :icon="personOutline" aria-hidden="true"></ion-icon>
+            </ion-button>
           </ion-col>
           <ion-col size="1">
-            cart(0)
+            <ion-button color="light" aria-label="Cart">
+              cart({{ cartItemsCount }})
+              <ion-icon :icon="cart" aria-hidden="true"></ion-icon>
+            </ion-button>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -43,5 +49,32 @@
 </template>
 
 <script setup lang="ts">
-import { IonHeader, IonTitle, IonToolbar, IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { IonHeader, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonIcon, IonButton } from '@ionic/vue';
+import { cart, personOutline } from 'ionicons/icons';
+import { ref } from 'vue';
+
+const cartItemsCount = ref(0)
 </script>
+
+<style lang="scss" scoped>
+* {
+  font-weight: bold;
+  font-size: 12px;
+}
+
+ion-button {
+  --background: #23242f;
+  --color: #fff;
+
+  font-size: 9px;
+
+  ion-icon {
+    margin-left: 4px;
+    font-size: 16px;
+  }
+}
+
+ion-title {
+  padding: 0;
+}
+</style>
