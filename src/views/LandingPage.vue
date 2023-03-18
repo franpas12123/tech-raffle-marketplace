97032d9504f5fb0f653806fadf11efddd3393d90
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-18 18:26:05 +0800

    add top banner and closing soon template

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,11 +1,33 @@
 <template>
   <Layout>
     <template v-slot:content>
-      <h1 class="ion-text-center">landing page</h1>
+      <!-- Top Banner -->
+      <TopBanner class="ion-margin-top"></TopBanner>
+
+      <div class="closing-soon">
+        <!-- <h1 class="ion-text-center ion-text-uppercase">
+          <b>closing soon</b>
+        </h1> -->
+        <!-- <SwiperSlide></SwiperSlide> -->
+      </div>
     </template>
   </Layout>
 </template>
 
 <script setup lang="ts">
+import { IonImg } from '@ionic/vue';
 import Layout from '@/layouts/DefaultLayout.vue';
+
+import TopBanner from '@/components/landing/TopBanner.vue';
+import SwiperSlide from '@/components/landing/SwiperSlide.vue';
 </script>
+
+<style lang="scss" scoped>
+.closing-soon {
+  height: 350px;
+
+  h1 b {
+    font-weight: 800;
+  }
+}
+</style>
