2003d8d9e00ea8d0973d3b6d1f9bc9db3584f250
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-25 23:25:53 +0800

    create user details layout

---
+++ src/views/UserDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <UserDetailsLayout>
+    <router-view></router-view>
+  </UserDetailsLayout>
+</template>
+
+<script setup lang="ts">
+import UserDetailsLayout from '@/layouts/UserDetailsLayout.vue';
+
+</script>
+
+<style lang="scss" scoped></style>
