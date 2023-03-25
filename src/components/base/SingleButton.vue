fd6f0a981b1d56149b892ccbccd65cd045fc5203
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-25 23:27:37 +0800

    add single button component

---
+++ src/components/base/SingleButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-row class="ion-margin-top ion-margin-left">
+    <ion-button :color="color" type="submit" :disabled="disabled" @click="$emit('clicked')">
+      <ion-icon v-if="icon" slot="start" :icon="icon"></ion-icon>
+      {{ text }}
+    </ion-button>
+  </ion-row>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    default: null
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  color: {
+    default: 'light'
+  },
+  text: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<style scoped></style>
