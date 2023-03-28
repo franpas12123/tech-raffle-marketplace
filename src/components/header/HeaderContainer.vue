7efafb68da4b9f00c58b357f10c3995b0fa72f84
Frances Pasulohan <francespasulohan@gmail.com>
2023-03-29 00:53:34 +0800

    fix header responsiveness

--- src/components/header/HeaderContainer.vue
+++ src/components/header/HeaderContainer.vue
@@ -1,47 +1,64 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-buttons slot="start">
-        <ion-button color="dark" class="logo" @click="() => router.push('/')">
-          <ion-img class="banner" :src="`./assets/icons/rapol-logo.svg`" alt=""></ion-img>
-        </ion-button>
-      </ion-buttons>
-      <ion-buttons class="nav-links">
-        <ion-button @click="() => router.push('/prizes')">
-          Prizes
-        </ion-button>
-        <ion-button @click="() => router.push('/products')">
-          Products
-        </ion-button>
-        <ion-button @click="() => router.push('/winners')">
-          Winners
-        </ion-button>
-        <ion-button @click="() => router.push('/faq')">
-          FAQ
-        </ion-button>
-        <ion-button @click="() => router.push('/charity')">
-          Charity
-        </ion-button>
-      </ion-buttons>
-      <ion-buttons slot="end">
-        <!-- @Todo: remove href later -->
-        <ion-button aria-label="Login/Signup" @click="() => router.push('/login')" href="/login">
-          login / signup
-          <ion-icon :src="`${assets}/user-transparent.svg`" aria-hidden="true"></ion-icon>
-        </ion-button>
-      </ion-buttons>
-      <ion-buttons slot="end">
-        <ion-button aria-label="Cart">
-          cart({{ cartItemsCount }})
-          <ion-icon :src="`${assets}/cart-transparent.svg`" aria-hidden="true"></ion-icon>
-        </ion-button>
-      </ion-buttons>
+      <ion-grid fixed>
+        <ion-row class="ion-align-items-center">
+          <ion-col class="logo" size="4" size-md="2.5" size-lg="2">
+            <ion-button @click="() => router.push('/')" fill="clear">
+              <ion-img class="banner" :src="`./assets/icons/rapol-logo.svg`" alt=""></ion-img>
+            </ion-button>
+          </ion-col>
+          <ion-col class="ion-hide-lg-down">
+            <ion-buttons class="nav-links">
+              <ion-button @click="() => router.push('/prizes')">
+                Prizes
+              </ion-button>
+              <ion-button @click="() => router.push('/products')">
+                Products
+              </ion-button>
+              <ion-button @click="() => router.push('/winners')">
+                Winners
+              </ion-button>
+              <ion-button @click="() => router.push('/faq')">
+                FAQ
+              </ion-button>
+              <ion-button @click="() => router.push('/charity')">
+                Charity
+              </ion-button>
+            </ion-buttons>
+          </ion-col>
+          <ion-col>
+            <ion-row class="ion-justify-content-end">
+              <ion-buttons slot="">
+                <!-- @Todo: remove href later -->
+                <ion-button aria-label="Login/Signup" @click="() => router.push('/login')" href="/login">
+                  login / signup
+                  <ion-icon :src="`${assets}/user-transparent.svg`" aria-hidden="true"></ion-icon>
+                </ion-button>
+                <ion-button aria-label="Cart">
+                  cart({{ cartItemsCount }})
+                  <ion-icon :src="`${assets}/cart-transparent.svg`" aria-hidden="true"></ion-icon>
+                </ion-button>
+              </ion-buttons>
+            </ion-row>
+
+          </ion-col>
+          <!-- <ion-col>
+            <ion-buttons slot="">
+              <ion-button aria-label="Cart">
+                cart({{ cartItemsCount }})
+                <ion-icon :src="`${assets}/cart-transparent.svg`" aria-hidden="true"></ion-icon>
+              </ion-button>
+            </ion-buttons>
+          </ion-col> -->
+        </ion-row>
+      </ion-grid>
     </ion-toolbar>
   </ion-header>
 </template>
 
 <script setup lang="ts">
-import { IonHeader, IonToolbar, IonIcon, IonButton, IonButtons } from '@ionic/vue';
+import { IonHeader, IonToolbar, IonIcon, IonButton, IonButtons, IonGrid } from '@ionic/vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -80,13 +97,15 @@ ion-button {
 }
 
 .logo {
+  min-width: 100px;
   width: 100px;
 
   ion-button {
-    border-radius: 0 !important;
+    --background: transparent !important;
   }
 
   ion-img {
+    width: 100px;
     padding: 10px;
   }
 }
