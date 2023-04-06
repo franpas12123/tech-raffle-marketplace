ba3b6d6256634e9cb1a4c14c1c559f9ebfa7b1b3
Frances Pasulohan <francespasulohan@gmail.com>
2023-04-06 23:48:22 +0800

    add interface for products and winners

---
+++ src/types/product.d.ts
@@ -0,0 +1,11 @@
+declare interface Product {
+  title: string;
+  productImgSrc: string;
+  details: string;
+  price: number;
+}
+
+declare interface Winners extends Product {
+    ticketNumber: string,
+    announcedOn: Date,
+}
