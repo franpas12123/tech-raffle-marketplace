a49c45436fc8046bc8a0642ca37b5840706cbb2f
Frances Pasulohan <francespasulohan@gmail.com>
2023-06-01 18:08:42 +0800

    add payment method dictionary

--- src/utils/dictionary.ts
+++ src/utils/dictionary.ts
@@ -1,7 +1,18 @@
 const dictionary = {
   genericError: {
     fetch: 'Error fetching'
-  }
+  },
+  paymentMethods: [
+    {
+      name: 'GCash',
+    },
+    {
+      name: 'Visa',
+    },
+    {
+      name: 'Mastercard',
+    }
+  ]
 }
 
 export default dictionary
