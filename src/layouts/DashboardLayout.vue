51b9489cb0113fe5731a52cd649317fe4f7569c7
Frances Pasulohan <francespasulohan@gmail.com>
2023-06-09 22:53:00 +0800

    add other user and admin menu components

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -26,8 +26,6 @@ import Layout from "./DefaultLayout.vue"
 import { computed, defineAsyncComponent } from 'vue';
 
 import DashboardMenu from "@/layouts/DashboardMenu.vue"
-// import changePassword from "@/components/user_details/ChangePassword.vue"
-// import myAddress from "@/components/user_details/MyAddress.vue"
 
 const props = defineProps({
   userType: {
@@ -40,27 +38,37 @@ const route = useRoute()
 const path = computed(() => route.name)
 const store = useStore()
 
-console.log('route', route)
+console.log('route', path, route, route.name)
 
 const substr = props.userType === 'user' ? '/user/:id/' : '/admin/:id/'
 const routeMatch = useRouter().getRoutes().filter(r => r.path.includes(substr) && r.name === route.name)
 
-const componentName = routeMatch.map(r => r.name)[0]
+const componentName = route.name === 'userDetails' ? 'userDetails' : routeMatch.map(r => r.name)[0]
+console.log('componentName', componentName)
 const userMenuComponents = {
+  userDetails: defineAsyncComponent(() => import('@/components/user_details/PersonalDetails.vue')),
+  personalDetails: defineAsyncComponent(() => import('@/components/user_details/PersonalDetails.vue')),
+  myAddress: defineAsyncComponent(() => import('@/components/user_details/MyAddress.vue')),
+  paymentOptions: defineAsyncComponent(() => import('@/components/user_details/PaymentOptions.vue')),
   changePassword: defineAsyncComponent(() => import('@/components/user_details/ChangePassword.vue')),
-  myAddress: () => import('@/components/user_details/MyAddress.vue')
 }
 
 const adminMenuComponents = {
+  // products
   adminProducts: defineAsyncComponent(() => import('@/components/admin/products/ProductsIndex.vue')),
   editProducts: defineAsyncComponent(() => import('@/components/admin/products/AddEditProducts.vue')),
   addProducts: defineAsyncComponent(() => import('@/components/admin/products/AddEditProducts.vue')),
+  // categories
   adminCategories: defineAsyncComponent(() => import('@/components/admin/categories/CategoriesIndex.vue')),
   addCategories: defineAsyncComponent(() => import('@/components/admin/categories/AddEditCategories.vue')),
+  // campaigns
+  adminCampaigns: defineAsyncComponent(() => import('@/components/admin/campaigns/CampaignsIndex.vue')),
+  editCampaigns: defineAsyncComponent(() => import('@/components/admin/campaigns/AddEditCampaigns.vue')),
+  addCampaigns: defineAsyncComponent(() => import('@/components/admin/campaigns/AddEditCampaigns.vue')),
 }
 
 const computedMenuComponents = computed(() => {
-  console.log('computedMenuComponents', adminMenuComponents)
+  console.log('computedMenuComponents', props.userType === 'admin' ? adminMenuComponents : userMenuComponents)
   return props.userType === 'admin' ? adminMenuComponents : userMenuComponents
 })
 </script>
