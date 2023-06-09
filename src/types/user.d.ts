6e388020bb7e8a6535e31013508ab2c5009c8ebe
Frances Pasulohan <francespasulohan@gmail.com>
2023-06-09 23:00:27 +0800

    add user and inputs interface

---
+++ src/types/user.d.ts
@@ -0,0 +1,10 @@
+// This file is for importing typescript types for the inputs
+
+declare interface UserDetails {
+  first_name: string
+  last_name: string;
+  email: string,
+  date_of_birth: string
+  number: string,
+  nationality: string
+}
