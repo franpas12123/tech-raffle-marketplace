47974ba76a4d001715d5ab9c5ef8928deb171cb6
Frances Pasulohan <francespasulohan@gmail.com>
2023-06-16 01:13:34 +0800

    currently disable add user button

--- src/components/admin/users/UsersIndex.vue
+++ src/components/admin/users/UsersIndex.vue
@@ -6,7 +6,7 @@
           <h1 class="title">Users</h1>
         </ion-col>
         <ion-col size="3.5">
-          <ion-button expand="block" @click="() => router.push({ name: 'addUsers' })">Add User</ion-button>
+          <ion-button disabled expand="block" @click="() => router.push({ name: 'addUsers' })">Add User</ion-button>
         </ion-col>
         <ion-col size="3.5">
           <ion-button expand="block">Export</ion-button>
